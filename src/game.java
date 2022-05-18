15db975178623c0125b5ae20ef0aa80c292884dd
sussy-balls-impostor <eo2dog192@gmail.com>
2022-05-18 16:46:49 +0200

    gfhhhhhhhh

--- src/game.java
+++ src/game.java
@@ -1,26 +1,21 @@
 package src;
 import ea.*;
 import ea.edu.AnzeigeE;
+import ea.edu.SPIEL;
 import ea.internal.gui.Fenster;
 
-class game {
+class game extends SPIEL {
     static AnzeigeE window;
     static DREIECK player;
     public static void main(String[] args) {
         window = new AnzeigeE(1280, 720);
         player = new DREIECK();
         player.setzeSichtbar(true);
-        tick();
-
+        game test = new game();
     }
 
-    static void tick(){
-        player.verschiebenUm(10,0);
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        tick();
+
+    public void tick() {
+        player.verschiebenUm(200,0);
     }
 }
