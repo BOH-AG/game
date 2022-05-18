8bb50e39d35ca032510a036e87dfbebe46bb8055
sussy-balls-impostor <eo2dog192@gmail.com>
2022-05-18 15:56:22 +0200

    fgtgggggggggggg

--- src/game.java
+++ src/game.java
@@ -1,12 +1,14 @@
 package src;
 import ea.*;
+//import ea.internal.gui.Fenster;
+import ea.edu.AnzeigeE;
 import ea.internal.gui.Fenster;
 
-class main {
+class game {
+    static AnzeigeE window;
     static DREIECK player;
-    static Fenster window;
     public static void main(String[] args) {
-        window = new Fenster(1280, 720, "Bohline Detroit", false, 20, 20);
+        window = new AnzeigeE(1280, 720);
         player = new DREIECK();
         player.setzeSichtbar(true);
     }
