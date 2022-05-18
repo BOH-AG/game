223699d4a2a22d092e23e41e82e08e4f1c29fb0f
sussy-balls-impostor <eo2dog192@gmail.com>
2022-05-18 22:20:39 +0200

    fuck spiel, all my homie use game

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 package src;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
         game sus = new game();
-        sus.Game();
     }
 }
--- src/game.java
+++ src/game.java
@@ -1,42 +1,46 @@
 package src;
-import ea.TastenLosgelassenReagierbar;
-import ea.edu.AnzeigeE;
-import ea.edu.SPIEL;
-import ea.Game;
+import ea.*;
 
+public class game extends Game implements TastenLosgelassenReagierbar, Ticker {
 
-public class game extends SPIEL {
-    AnzeigeE window;
-    Keys k = new Keys();
-    //DREIECK player;
-    void Game() {
-        window = new AnzeigeE(1280, 720);
-        //player = new DREIECK();
-        //player.setzeSichtbar(true);
-        //tickerStarten(100);
-        window.tastenReagierbarAnmelden(k);
+    Dreieck player;
+    Bild sussyicon;
+    Maus diesendungmitder;
+
+    public game() {
+        super(1280, 720, "Neu Köln Simulator");
+        Punkt top = new Punkt(100f, 100f);
+        Punkt bl = new Punkt(75f, 150f);
+        Punkt br = new Punkt(125f, 150f);
+        sussyicon = new Bild("rsc/disc.png");
+        diesendungmitder = new Maus(Maus.TYPE_FADENKREUZ);
+        player = new Dreieck(top, bl, br);
+        player.farbeSetzen("Rot");
+        wurzel.add(player);
+        iconSetzen(sussyicon);
+        mausAnmelden(diesendungmitder);
+        tastenReagierbarAnmelden(this);
+        tastenLosgelassenReagierbarAnmelden(this);
+        tickerAnmelden(this, 17);
     }
 
-   /*Override
+    @Override
+    public void tasteReagieren(int code) {}
+    @Override
+    public void tasteLosgelassen(int code) {}
+
     public void tick() {
-        super.tick();
-
-    }*/
-
-
-    /*@Override
-    public void tasteReagieren(int key) {
-        super.tasteReagieren(key);
-        if (key==22) player.verschiebenUm(0,-10); // w 22
-        if (key==3) player.verschiebenUm(10,0); //a 3
-        if (key==18) player.verschiebenUm(0, 10); //s 18
-        if (key==0) player.verschiebenUm(-10,0); //d 0
-
-    }*/
-    
-
-
-
-
-
+        if(tasteGedrueckt(Taste.W)) {
+            player.verschieben(0, -4);
+        }
+        if(tasteGedrueckt(Taste.D)) {
+            player.verschieben(4, 0);
+        }
+        if(tasteGedrueckt(Taste.S)) {
+            player.verschieben(0, 4);
+        }
+        if(tasteGedrueckt(Taste.A)) {
+            player.verschieben(-4, 0);
+        }
+    }
 }
