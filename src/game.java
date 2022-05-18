f0c4e6219a5ed990e3bdcbf3aa5963fba8a35650
Luca <luca.milde@gmx.net>
2022-05-18 16:13:20 +0200

    tick

--- src/game.java
+++ src/game.java
@@ -1,6 +1,5 @@
 package src;
 import ea.*;
-//import ea.internal.gui.Fenster;
 import ea.edu.AnzeigeE;
 import ea.internal.gui.Fenster;
 
@@ -11,5 +10,17 @@ class game {
         window = new AnzeigeE(1280, 720);
         player = new DREIECK();
         player.setzeSichtbar(true);
+        tick();
+
+    }
+
+    static void tick(){
+        player.verschiebenUm(10,0);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        tick();
     }
 }
