1984b267eaedce9b9cee31116439be3d674ab70b
sussy-balls-impostor <eo2dog192@gmail.com>
2022-05-18 17:48:33 +0200

    fkjdehwslifkjhfkjhfhjkfdkfd help

--- src/game.java
+++ src/game.java
@@ -1,19 +1,46 @@
 package src;
+import ea.TastenLosgelassenReagierbar;
 import ea.edu.AnzeigeE;
 import ea.edu.SPIEL;
 
-class game extends SPIEL {
+public class game extends SPIEL {
     AnzeigeE window;
     DREIECK player;
     void Game() {
         window = new AnzeigeE(1280, 720);
         player = new DREIECK();
         player.setzeSichtbar(true);
-        tickerStarten(17);
+        tickerStarten(100);
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+
+    }
+
+    // w 22
+    //a 3
+    //s 18
+    //d 0
+
+    @Override
+    public void tasteReagieren(int key) {
+        super.tasteReagieren(key);
+        if (key==22) {
+            player.verschiebenUm(0,-10);
+        }
+        if (key==3) {
+            player.verschiebenUm(10,0);
+        }
+        if (key==0) {
+            player.verschiebenUm(-10,0);
+        }
+        if (key==18) {
+            player.verschiebenUm(0,10);
+        }
+
     }
 
 
-    void tick() {
-        player.verschiebenUm(200,0);
-    }
 }
