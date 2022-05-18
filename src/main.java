70d250fb16736a9b92d05a927fdc82b8db94ac43
sussy-balls-impostor <eo2dog192@gmail.com>
2022-05-18 15:33:17 +0200

    ferwfffe

--- src/main.java
+++ src/main.java
@@ -1,10 +1,13 @@
 package src;
 import ea.*;
+import ea.internal.gui.Fenster;
 
 class main {
     static DREIECK player;
+    static Fenster window;
     public static void main(String[] args) {
-
+        window = new Fenster(1280, 720, "Bohline Detroit", false, 20, 20);
         player = new DREIECK();
+        player.setzeSichtbar(true);
     }
 }
